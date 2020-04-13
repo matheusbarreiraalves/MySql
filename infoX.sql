4cc40223cc6d8c96835be5becb98278e63f5f611
Matheus Barreira Alves <52544520+matheusbarreiraalves@users.noreply.github.com>
2020-04-13 15:29:15 -0300

    Add files via upload
    Adicionado o Relacionamento de Tabelas// Aula #04

--- infoX.sql
+++ infoX.sql
@@ -50,3 +50,32 @@ values('linus torvalds', 'rua tux, 2015', '9999-9999', 'linus@linux.com','nova y
 
 select * from tbclientes;
 
+use dbinfox;
+
+create table tbos(
+os int primary key auto_increment,
+data_os timestamp default current_timestamp,
+equipamento  varchar(150) not null,
+defeito varchar(150) not null,
+servico varchar(150),
+tecnico varchar(30),
+valor decimal(10,2),
+idcli int not null,
+foreign key(idcli) references tbclientes(idcli)
+);
+
+describe tbos;
+
+insert into tbos (equipamento,defeito,servico,tecnico,valor,idcli)
+values ('notebook','não liga','troca da fonte','Matheus',87.50,1);
+
+select * from tbos
+
+
+select 
+O.os,equipamento,defeito,servico,valor,
+C.nomecli,fonecli
+from tbos as O
+inner join tbclientes as C
+on (O.idcli = C.idcli);
+
